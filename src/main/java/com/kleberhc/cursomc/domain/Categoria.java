77f26d0ad55848555106ebd5419d62f2b3b333c4
Kleber Hochwart <hc.kleber@gmail.com>
2018-05-27 16:57:11 -0300

    Produto e assoc many2many

--- src/main/java/com/kleberhc/cursomc/domain/Categoria.java
+++ src/main/java/com/kleberhc/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.kleberhc.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -18,6 +21,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy="categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 	}
@@ -44,6 +50,15 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -68,6 +83,7 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
+
 	
 	
 	
