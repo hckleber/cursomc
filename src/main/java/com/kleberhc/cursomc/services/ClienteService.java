87feabc38b9ee7a10451cdb9cf3791eb3d5cd814
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-01 16:15:42 -0300

    Endpoint/clientes/id disponível

---
+++ src/main/java/com/kleberhc/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.kleberhc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kleberhc.cursomc.domain.Cliente;
+import com.kleberhc.cursomc.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: "+ id + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
