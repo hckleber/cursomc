b6a8bd293bc6cd892fb24cfe732436d5fad3f1dd
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-01 15:49:18 -0300

    Implementaçao Cliente, telefones e endereço

--- src/main/java/com/kleberhc/cursomc/CursomcApplication.java
+++ src/main/java/com/kleberhc/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kleberhc.cursomc.domain.Categoria;
 import com.kleberhc.cursomc.domain.Cidade;
+import com.kleberhc.cursomc.domain.Cliente;
+import com.kleberhc.cursomc.domain.Endereco;
 import com.kleberhc.cursomc.domain.Estado;
 import com.kleberhc.cursomc.domain.Produto;
+import com.kleberhc.cursomc.enums.TipoCliente;
 import com.kleberhc.cursomc.repositories.CategoriaRepository;
 import com.kleberhc.cursomc.repositories.CidadeRepository;
+import com.kleberhc.cursomc.repositories.ClienteRepository;
+import com.kleberhc.cursomc.repositories.EnderecoRepository;
 import com.kleberhc.cursomc.repositories.EstadoRepository;
 import com.kleberhc.cursomc.repositories.ProdutoRepository;
 
@@ -27,7 +32,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -70,6 +78,16 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null,  "Maria Silva", "maria@gmail.com", "123456789", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("123456", "987654"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardins", "6542318", cli1, c2);
+		Endereco e2 = new Endereco(null, "Av. Matos", "105", "Sala 800", "Centro", "4632154", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.save(Arrays.asList(cli1));
+		enderecoRepository.save(Arrays.asList(e1, e2));
 		
 		
 		
---
+++ src/main/java/com/kleberhc/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.kleberhc.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kleberhc.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
