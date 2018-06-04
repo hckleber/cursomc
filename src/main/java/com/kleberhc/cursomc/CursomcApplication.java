2d44a6569295903028d228fe9b4bcdd689f0d96c
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-04 08:50:43 -0300

    ItemPedido ItemPedidoPK

--- src/main/java/com/kleberhc/cursomc/CursomcApplication.java
+++ src/main/java/com/kleberhc/cursomc/CursomcApplication.java
@@ -13,6 +13,8 @@ import com.kleberhc.cursomc.domain.Cidade;
 import com.kleberhc.cursomc.domain.Cliente;
 import com.kleberhc.cursomc.domain.Endereco;
 import com.kleberhc.cursomc.domain.Estado;
+import com.kleberhc.cursomc.domain.ItemPedido;
+import com.kleberhc.cursomc.domain.ItemPedidoPK;
 import com.kleberhc.cursomc.domain.Pagamento;
 import com.kleberhc.cursomc.domain.PagamentoComBoleto;
 import com.kleberhc.cursomc.domain.PagamentoComCartao;
@@ -25,6 +27,7 @@ import com.kleberhc.cursomc.repositories.CidadeRepository;
 import com.kleberhc.cursomc.repositories.ClienteRepository;
 import com.kleberhc.cursomc.repositories.EnderecoRepository;
 import com.kleberhc.cursomc.repositories.EstadoRepository;
+import com.kleberhc.cursomc.repositories.ItemPedidoRepository;
 import com.kleberhc.cursomc.repositories.PagamentoRepository;
 import com.kleberhc.cursomc.repositories.PedidoRepository;
 import com.kleberhc.cursomc.repositories.ProdutoRepository;
@@ -48,6 +51,8 @@ public class CursomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -115,6 +120,20 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.save(Arrays.asList(ped1, ped2));
 		pagamentoRepository.save(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.save(Arrays.asList(ip1, ip2, ip3));
+		
 		
 	}
 	
--- src/main/java/com/kleberhc/cursomc/domain/Pedido.java
+++ src/main/java/com/kleberhc/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.kleberhc.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -9,8 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -37,6 +39,9 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {
 		
@@ -89,6 +94,14 @@ public class Pedido implements Serializable{
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
 
 	@Override
 	public int hashCode() {
@@ -114,5 +127,7 @@ public class Pedido implements Serializable{
 			return false;
 		return true;
 	}
+
+
 	
 }
