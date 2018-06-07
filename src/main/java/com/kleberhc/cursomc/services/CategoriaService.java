ef820d8f1207e882f12fed0bc2ca3b5dff4077e5
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-07 17:10:50 -0300

    Lista todas as categorias com DTO

--- src/main/java/com/kleberhc/cursomc/services/CategoriaService.java
+++ src/main/java/com/kleberhc/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.kleberhc.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -40,6 +42,10 @@ public class CategoriaService {
 		}catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityExeption("Não é possível excluir uma categoria que possui produtos");
 					}
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 	
 }
