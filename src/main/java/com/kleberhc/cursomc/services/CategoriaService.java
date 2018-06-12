98279a1729f086309c29cdaaa8f89d02a3293482
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-12 12:42:11 -0300

    Paginação com parametros adicionais na request

--- src/main/java/com/kleberhc/cursomc/services/CategoriaService.java
+++ src/main/java/com/kleberhc/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kleberhc.cursomc.domain.Categoria;
@@ -46,6 +49,12 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	// Cria paginação para retorno de categoria no JSON
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
