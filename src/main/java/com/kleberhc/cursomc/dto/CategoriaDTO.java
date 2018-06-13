51dc80c9e49874f768999e685ec63a9c25682839
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-13 15:17:55 -0300

    Validação com bean validation

--- src/main/java/com/kleberhc/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/kleberhc/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,9 @@ package com.kleberhc.cursomc.dto;
 
 import java.io.Serializable;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import com.kleberhc.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
@@ -12,6 +15,9 @@ public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório!")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 	
 	
--- src/main/java/com/kleberhc/cursomc/services/CategoriaService.java
+++ src/main/java/com/kleberhc/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.kleberhc.cursomc.domain.Categoria;
+import com.kleberhc.cursomc.dto.CategoriaDTO;
 import com.kleberhc.cursomc.repositories.CategoriaRepository;
 import com.kleberhc.cursomc.services.exceptions.DataIntegrityExeption;
 import com.kleberhc.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
