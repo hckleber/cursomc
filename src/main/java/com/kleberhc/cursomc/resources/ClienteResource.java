eaf22bc0ecb05fa573a0f35c326f8a1d0c8a7504
Kleber Hochwart <hc.kleber@gmail.com>
2018-06-22 16:22:39 -0300

    Inserindo novo clinete com POST

--- src/main/java/com/kleberhc/cursomc/resources/ClienteResource.java
+++ src/main/java/com/kleberhc/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.kleberhc.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kleberhc.cursomc.domain.Cliente;
 import com.kleberhc.cursomc.dto.ClienteDTO;
+import com.kleberhc.cursomc.dto.ClienteNewDTO;
 import com.kleberhc.cursomc.services.ClienteService;
 
 @RestController
@@ -26,6 +29,19 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 	
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){ // RequestBody faz o JSON ser convertido em obj JAVA
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		
+		//Caso de criação de uma nova categoria com POST
+		//A resposta para um POST no http deve retornar o URI + o ID criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
--- src/main/java/com/kleberhc/cursomc/services/ClienteService.java
+++ src/main/java/com/kleberhc/cursomc/services/ClienteService.java
@@ -1,28 +1,22 @@
 package com.kleberhc.cursomc.services;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import com.kleberhc.cursomc.domain.Cidade;
 import com.kleberhc.cursomc.domain.Cliente;
-import com.kleberhc.cursomc.domain.Cliente;
+import com.kleberhc.cursomc.domain.Endereco;
 import com.kleberhc.cursomc.dto.ClienteDTO;
-import com.kleberhc.cursomc.dto.ClienteDTO;
+import com.kleberhc.cursomc.dto.ClienteNewDTO;
+import com.kleberhc.cursomc.enums.TipoCliente;
 import com.kleberhc.cursomc.repositories.ClienteRepository;
+import com.kleberhc.cursomc.repositories.EnderecoRepository;
 import com.kleberhc.cursomc.services.exceptions.DataIntegrityExeption;
 import com.kleberhc.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -31,6 +25,17 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null); // Usar nulo para dizer ao banco que é uma inclusão
+		obj = repo.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
+		return obj;
+	}
+	
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
@@ -68,6 +73,24 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente (null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		// vincula endereco ao cliente
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!=null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!=null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
+	}
+	
 	
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
