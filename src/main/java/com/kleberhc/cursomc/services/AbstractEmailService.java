d12ff11bf05b155baaefc0fadc3436b07f80dff3
hckleber <hc.kleber@gmail.com>
2020-04-18 20:47:22 -0300

    Email Html

--- src/main/java/com/kleberhc/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/kleberhc/cursomc/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.kleberhc.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.kleberhc.cursomc.domain.Pedido;
 
@@ -12,6 +20,15 @@ public abstract class AbstractEmailService implements EmailService{
 	@Value("${default.sender}")
 	private String sender;
 	
+	//Processa o templete do Html
+	@Autowired
+	private TemplateEngine templateEngine;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	
+	// Email em texto plano
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
@@ -28,4 +45,40 @@ public abstract class AbstractEmailService implements EmailService{
 		sm.setText(obj.toString());
 		return sm;
 	}
+	
+	
+	protected String htmlFromTemplatePedido(Pedido obj) {
+		Context context = new Context();
+		//Cria uma passagem de objeto para o template do html através de um "apelido"
+		context.setVariable("pedido", obj);
+		//aponta para pasta com as configuraçoes do template
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
+	
+	
+	
+	
+	//  Email em Html
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+		try {
+		MimeMessage mm = prepareMimeMessageFromPedido(obj);
+		sendHtmlEmail(mm);
+		}
+		catch (MessagingException e) {
+			sendOrderConfirmationEmail(obj);
+		}
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+		mmh.setTo(obj.getCliente().getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText(htmlFromTemplatePedido(obj), true);
+		return mimeMessage;
+	}
+	
 }
--- src/main/java/com/kleberhc/cursomc/services/EmailService.java
+++ src/main/java/com/kleberhc/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.kleberhc.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.kleberhc.cursomc.domain.Pedido;
@@ -10,5 +12,7 @@ public interface EmailService {
 	
 	void sendEmail(SimpleMailMessage msg) ;
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
+	void sendHtmlEmail(MimeMessage msg); 
 }
