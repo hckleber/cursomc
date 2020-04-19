2a00b296344e22438144af66132fdf9655a8e9f0
hckleber <hc.kleber@gmail.com>
2020-04-19 19:27:17 -0300

    Implementando autorizacao

--- src/main/java/com/kleberhc/cursomc/config/SecurityConfig.java
+++ src/main/java/com/kleberhc/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.kleberhc.cursomc.security.JWTAuthenticationFilter;
+import com.kleberhc.cursomc.security.JWTAuthorizationFilter;
 import com.kleberhc.cursomc.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MACHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
