a900f88b2b053fc768ab5c42b9affa4cf1c67c16
hckleber <hc.kleber@gmail.com>
2020-04-22 13:50:06 -0300

    Esqueci a senha

--- src/main/java/com/kleberhc/cursomc/resources/AuthResource.java
+++ src/main/java/com/kleberhc/cursomc/resources/AuthResource.java
@@ -1,30 +1,42 @@
 package com.kleberhc.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kleberhc.cursomc.dto.EmailDTO;
 import com.kleberhc.cursomc.security.JWTUtil;
 import com.kleberhc.cursomc.security.UserSS;
+import com.kleberhc.cursomc.services.AuthService;
 import com.kleberhc.cursomc.services.UserService;
 
 @RestController
-@RequestMapping(value="/auth")
+@RequestMapping(value = "/auth")
 public class AuthResource {
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
-	
-	@RequestMapping(value="/refresh_token", method=RequestMethod.POST)
-	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
+
+	@Autowired
+	private AuthService service;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
-		response.addHeader("Authorization", "Bearer "+ token);
+		response.addHeader("Authorization", "Bearer " + token);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDTO) {
+		service.sendNewPassword(objDTO.getEmail());
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/kleberhc/cursomc/services/EmailService.java
+++ src/main/java/com/kleberhc/cursomc/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.kleberhc.cursomc.domain.Cliente;
 import com.kleberhc.cursomc.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,8 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg); 
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
+	
+	
 }
